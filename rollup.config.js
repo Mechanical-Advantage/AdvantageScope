788ce9be74b0e776ef7d7dd534240e5a2a065d01
Jonah <47046556+jonahb55@users.noreply.github.com>
2022-12-24 16:58:33 -0500

    Split rollup bundle to reduce memory usage

--- rollup.config.js
+++ rollup.config.js
@@ -13,20 +13,31 @@ const bundle = (input, output, external = []) => ({
   external: external
 });
 
-export default [
+const mainBundles = [
   bundle("main/main.ts", "main.js", ["electron", "electron-fetch", "fs", "jsonfile", "net", "os", "path", "ssh2"]),
-  bundle("preload.ts", "preload.js", ["electron"]),
-
-  bundle("hub/hub.ts", "hub.js"),
-  bundle("hub/dataSources/rlogWorker.ts", "hub$rlogWorker.js"),
-  bundle("hub/dataSources/wpilogWorker.ts", "hub$wpilogWorker.js"),
-  bundle("hub/dataSources/dsLogWorker.ts", "hub$dsLogWorker.js"),
-  bundle("hub/exportWorker.ts", "hub$exportWorker.js"),
+  bundle("preload.ts", "preload.js", ["electron"])
+];
+const largeRendererBundles = [bundle("hub/hub.ts", "hub.js"), bundle("satellite.ts", "satellite.js")];
+const smallRendererBundles = [
   bundle("editRange.ts", "editRange.js"),
   bundle("unitConversion.ts", "unitConversion.js"),
   bundle("renameTab.ts", "renameTab.js"),
   bundle("export.ts", "export.js"),
   bundle("download.ts", "download.js"),
-  bundle("satellite.ts", "satellite.js"),
   bundle("preferences.ts", "preferences.js")
 ];
+const workerBundles = [
+  bundle("hub/dataSources/rlogWorker.ts", "hub$rlogWorker.js"),
+  bundle("hub/dataSources/wpilogWorker.ts", "hub$wpilogWorker.js"),
+  bundle("hub/dataSources/dsLogWorker.ts", "hub$dsLogWorker.js"),
+  bundle("hub/exportWorker.ts", "hub$exportWorker.js")
+];
+
+export default (cliArgs) => {
+  if (cliArgs.configMain === true) return mainBundles;
+  if (cliArgs.configLargeRenderers === true) return largeRendererBundles;
+  if (cliArgs.configSmallRenderers === true) return smallRendererBundles;
+  if (cliArgs.configWorkers === true) return workerBundles;
+
+  return [...mainBundles, ...largeRendererBundles, ...smallRendererBundles, ...workerBundles];
+};
