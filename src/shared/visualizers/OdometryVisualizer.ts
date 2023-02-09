5425c26ec862af953703e8a0efd6c12708f5c26f
Jonah <47046556+jonahb55@users.noreply.github.com>
2023-02-09 00:48:57 -0500

    Add explicit bumper widths in inches

--- src/shared/visualizers/OdometryVisualizer.ts
+++ src/shared/visualizers/OdometryVisualizer.ts
@@ -155,7 +155,7 @@ export default class OdometryVisualizer implements Visualizer {
       command.poses.visionTarget.forEach((target: Pose2d) => {
         let robotPose = command.poses.robot[0];
         context.strokeStyle = "lightgreen";
-        context.lineWidth = pixelsPerInch;
+        context.lineWidth = 1 * pixelsPerInch; // 1 inch
         context.beginPath();
         context.moveTo(robotPos[0], robotPos[1]);
         context.lineTo(...calcCoordinates(target.translation));
@@ -194,7 +194,7 @@ export default class OdometryVisualizer implements Visualizer {
       gradient.addColorStop(1, "rgba(170, 170, 170, 0)");
 
       context.strokeStyle = gradient;
-      context.lineWidth = pixelsPerInch;
+      context.lineWidth = 1 * pixelsPerInch; // 1 inch
       context.lineCap = "round";
       context.lineJoin = "round";
       context.beginPath();
