ac3720b9f80e8968b8450ed2d836bb5840bfa76e
Jonah <47046556+jonahb55@users.noreply.github.com>
2023-02-17 17:53:30 -0500

    Add limits for extreme line graph values

--- src/shared/util.ts
+++ src/shared/util.ts
@@ -81,6 +81,11 @@ export function scaleValue(value: number, oldRange: [number, number], newRange: 
   return ((value - oldRange[0]) / (oldRange[1] - oldRange[0])) * (newRange[1] - newRange[0]) + newRange[0];
 }
 
+/** Clamps a value to a range. */
+export function clampValue(value: number, min: number, max: number) {
+  return Math.min(Math.max(value, min), max);
+}
+
 /**
  * Applys a transform to a pixel value.
  * @param originPx The origin pixel in image coordinates.
