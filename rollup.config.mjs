29ae259700acd207fbe1d4b34995372f8a37e7c6
Jonah <47046556+jonahb55@users.noreply.github.com>
2023-08-05 01:01:06 -0400

    Add build constants prototype

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,43 +1,79 @@
 import commonjs from "@rollup/plugin-commonjs";
 import nodeResolve from "@rollup/plugin-node-resolve";
+import replace from "@rollup/plugin-replace";
 import typescript from "@rollup/plugin-typescript";
 import cleanup from "rollup-plugin-cleanup";
 
-const bundle = (input, output, external = []) => ({
+const bundle = (input, output, isWpilib, external = []) => ({
   input: "src/" + input,
   output: {
     file: "bundles/" + output,
     format: "cjs"
   },
-  plugins: [typescript(), nodeResolve(), commonjs(), cleanup()],
+  plugins: [
+    typescript(),
+    nodeResolve(),
+    commonjs(),
+    cleanup(),
+    replace({
+      preventAssignment: true,
+      values: {
+        __distributor__: isWpilib ? "WPILib" : "Team6328",
+        __build_date__: new Date().toUTCString()
+      }
+    })
+  ],
   external: external
 });
 
-const mainBundles = [
-  bundle("main/main.ts", "main.js", ["electron", "electron-fetch", "fs", "jsonfile", "net", "os", "path", "ssh2"]),
-  bundle("preload.ts", "preload.js", ["electron"])
-];
-const largeRendererBundles = [bundle("hub/hub.ts", "hub.js"), bundle("satellite.ts", "satellite.js")];
-const smallRendererBundles = [
-  bundle("editRange.ts", "editRange.js"),
-  bundle("unitConversion.ts", "unitConversion.js"),
-  bundle("renameTab.ts", "renameTab.js"),
-  bundle("export.ts", "export.js"),
-  bundle("download.ts", "download.js"),
-  bundle("preferences.ts", "preferences.js")
-];
-const workerBundles = [
-  bundle("hub/dataSources/rlogWorker.ts", "hub$rlogWorker.js"),
-  bundle("hub/dataSources/wpilogWorker.ts", "hub$wpilogWorker.js"),
-  bundle("hub/dataSources/dsLogWorker.ts", "hub$dsLogWorker.js"),
-  bundle("hub/exportWorker.ts", "hub$exportWorker.js")
-];
+function mainBundles(isWpilib) {
+  return [
+    bundle("main/main.ts", "main.js", isWpilib, [
+      "electron",
+      "electron-fetch",
+      "fs",
+      "jsonfile",
+      "net",
+      "os",
+      "path",
+      "ssh2"
+    ]),
+    bundle("preload.ts", "preload.js", isWpilib, ["electron"])
+  ];
+}
+function largeRendererBundles(isWpilib) {
+  return [bundle("hub/hub.ts", "hub.js", isWpilib), bundle("satellite.ts", "satellite.js", isWpilib)];
+}
+function smallRendererBundles(isWpilib) {
+  return [
+    bundle("editRange.ts", "editRange.js", isWpilib),
+    bundle("unitConversion.ts", "unitConversion.js", isWpilib),
+    bundle("renameTab.ts", "renameTab.js", isWpilib),
+    bundle("export.ts", "export.js", isWpilib),
+    bundle("download.ts", "download.js", isWpilib),
+    bundle("preferences.ts", "preferences.js", isWpilib)
+  ];
+}
+function workerBundles(isWpilib) {
+  return [
+    bundle("hub/dataSources/rlogWorker.ts", "hub$rlogWorker.js", isWpilib),
+    bundle("hub/dataSources/wpilogWorker.ts", "hub$wpilogWorker.js", isWpilib),
+    bundle("hub/dataSources/dsLogWorker.ts", "hub$dsLogWorker.js", isWpilib),
+    bundle("hub/exportWorker.ts", "hub$exportWorker.js", isWpilib)
+  ];
+}
 
 export default (cliArgs) => {
-  if (cliArgs.configMain === true) return mainBundles;
-  if (cliArgs.configLargeRenderers === true) return largeRendererBundles;
-  if (cliArgs.configSmallRenderers === true) return smallRendererBundles;
-  if (cliArgs.configWorkers === true) return workerBundles;
+  const isWpilib = cliArgs.configWpilib;
+  if (cliArgs.configMain === true) return mainBundles(isWpilib);
+  if (cliArgs.configLargeRenderers === true) return largeRendererBundles(isWpilib);
+  if (cliArgs.configSmallRenderers === true) return smallRendererBundles(isWpilib);
+  if (cliArgs.configWorkers === true) return workerBundles(isWpilib);
 
-  return [...mainBundles, ...largeRendererBundles, ...smallRendererBundles, ...workerBundles];
+  return [
+    ...mainBundles(isWpilib),
+    ...largeRendererBundles(isWpilib),
+    ...smallRendererBundles(isWpilib),
+    ...workerBundles(isWpilib)
+  ];
 };
---
+++ src/shared/buildConstants.ts
@@ -0,0 +1,8 @@
+export enum Distributor {
+  Team6328,
+  WPILib
+}
+
+// @ts-ignore
+export const DISTRIBUTOR: Distributor = Distributor.__distributor__;
+export const BUILD_DATE: string = "__build_date__";
