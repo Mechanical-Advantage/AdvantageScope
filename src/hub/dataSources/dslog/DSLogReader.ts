d6b0569170b78a7944d1005e2a5d315440058c32
Jonah <47046556+jwbonner@users.noreply.github.com>
2023-10-15 19:33:56 -0400

    Fix PDH channel decoding

--- src/hub/dataSources/dslog/DSLogReader.ts
+++ src/hub/dataSources/dslog/DSLogReader.ts
@@ -58,113 +58,120 @@ export class DSLogReader {
     let timestamp = 0;
     let lastBatteryVolts = 0;
 
-    while (true) {
-      // Get basic fields
-      let mask = this.dataView.getUint8(position + 5);
-      let batteryVolts = this.dataView.getUint16(position + 2) / Math.pow(2, 8);
-      if (batteryVolts > 20) {
-        // Sometimes the battery voltage spikes at the end of the log, ignore that value
-        batteryVolts = lastBatteryVolts;
-      } else {
-        lastBatteryVolts = batteryVolts;
-      }
+    try {
+      while (true) {
+        // Get basic fields
+        let mask = this.dataView.getUint8(position + 5);
+        let batteryVolts = this.dataView.getUint16(position + 2) / Math.pow(2, 8);
+        if (batteryVolts > 20) {
+          // Sometimes the battery voltage spikes at the end of the log, ignore that value
+          batteryVolts = lastBatteryVolts;
+        } else {
+          lastBatteryVolts = batteryVolts;
+        }
 
-      // Create entry
-      let entry: DSLogEntry = {
-        timestamp: timestamp,
-        tripTimeMs: this.dataView.getUint8(position) * 0.5,
-        packetLoss: Math.min(Math.max(this.dataView.getInt8(position + 1) * 4 * 0.01, 0), 1),
-        batteryVolts: batteryVolts,
-        rioCpuUtilization: this.dataView.getUint8(position + 4) * 0.5 * 0.01,
-        brownout: (mask & (1 << 7)) === 0,
-        watchdog: (mask & (1 << 6)) === 0,
-        dsTeleop: (mask & (1 << 5)) === 0,
-        dsDisabled: (mask & (1 << 3)) === 0,
-        robotTeleop: (mask & (1 << 2)) === 0,
-        robotAuto: (mask & (1 << 1)) === 0,
-        robotDisabled: (mask & 1) === 0,
-        canUtilization: this.dataView.getUint8(position + 6) * 0.5 * 0.01,
-        wifiDb: this.dataView.getUint8(position + 7) * 0.5,
-        wifiMb: this.dataView.getUint16(position + 8) / Math.pow(2, 8),
-        powerDistributionCurrents: []
-      };
-      position += 10;
+        // Create entry
+        let entry: DSLogEntry = {
+          timestamp: timestamp,
+          tripTimeMs: this.dataView.getUint8(position) * 0.5,
+          packetLoss: Math.min(Math.max(this.dataView.getInt8(position + 1) * 4 * 0.01, 0), 1),
+          batteryVolts: batteryVolts,
+          rioCpuUtilization: this.dataView.getUint8(position + 4) * 0.5 * 0.01,
+          brownout: (mask & (1 << 7)) === 0,
+          watchdog: (mask & (1 << 6)) === 0,
+          dsTeleop: (mask & (1 << 5)) === 0,
+          dsDisabled: (mask & (1 << 3)) === 0,
+          robotTeleop: (mask & (1 << 2)) === 0,
+          robotAuto: (mask & (1 << 1)) === 0,
+          robotDisabled: (mask & 1) === 0,
+          canUtilization: this.dataView.getUint8(position + 6) * 0.5 * 0.01,
+          wifiDb: this.dataView.getUint8(position + 7) * 0.5,
+          wifiMb: this.dataView.getUint16(position + 8) / Math.pow(2, 8),
+          powerDistributionCurrents: []
+        };
+        position += 10;
 
-      // Get power distribution data
-      let pdType = getPDType(this.dataView.getUint8(position + 3));
+        // Get power distribution data
+        let pdType = getPDType(this.dataView.getUint8(position + 3));
+        position += 4;
+        let currents: number[] = [];
+        switch (pdType) {
+          case PowerDistributionType.REV:
+            // Skip CAN ID
+            position += 1;
 
-      let currents: number[] = [];
-      switch (pdType) {
-        case PowerDistributionType.REV:
-          position += 5;
-          // Read bytes
-          let ints: number[] = [];
-          for (let i = 0; i < 6; ++i) {
-            ints.push(this.dataView.getUint32(position, true));
+            // Convert to boolean array
+            let revBooleans: boolean[] = [];
+            this.data.subarray(position, position + 27).forEach((byte) => {
+              for (let i = 0; i < 8; i++) {
+                revBooleans.push((byte & (1 << i)) !== 0);
+              }
+            });
+            position += 27;
+
+            // Get main channel currents
+            for (let i = 0; i < 20; i++) {
+              let readPosition = Math.floor(i / 3) * 32 + (i % 3) * 10;
+              let value = 0;
+              for (let i = 0; i < 10; i++) {
+                value += revBooleans[readPosition + i] ? Math.pow(2, i) : 0;
+              }
+              currents.push(value / 8);
+            }
+
+            // Get extra channel channels
+            for (let i = 0; i < 4; i++) {
+              currents[i + 20] = this.data[position + i] / 16;
+            }
             position += 4;
-          }
-          let finalArrayREV = new Uint8Array(4);
-          finalArrayREV.set(this.data.subarray(position, position + 2), 0);
-          ints[6] = new DataView(finalArrayREV.buffer).getUint32(0);
-          position += 3;
-          let dataBytes = this.data.subarray(position, position + 4);
-          position += 4;
 
-          // Convert to final currents
-          for (let i = 0; i < 20; ++i) {
-            let dataIndex = Math.floor(i / 3);
-            let dataOffset = i % 3;
-            let data = ints[dataIndex];
-            let num = data << (32 - (dataOffset + 1) * 10);
-            num = num >>> 22;
-            currents[i] = num / 8;
-          }
-          for (let i = 0; i < 4; ++i) {
-            currents[i + 20] = dataBytes[i] / 16;
-          }
+            // Skip last byte (temperature data?)
+            position += 1;
+            break;
 
-          // Skip last byte (temperature data?)
-          position += 1;
-          break;
+          case PowerDistributionType.CTRE:
+            // Skip CAN ID
+            position += 1;
 
-        case PowerDistributionType.CTRE:
-          position += 5;
-          // Convert to boolean array
-          let booleanData: boolean[] = [];
-          this.data.subarray(position, position + 21).forEach((byte) => {
-            for (let i = 0; i < 8; i++) {
-              booleanData.push((byte & (1 << i)) !== 0);
+            // Convert to boolean array
+            let ctreBooleans: boolean[] = [];
+            this.data.subarray(position, position + 21).forEach((byte) => {
+              for (let i = 0; i < 8; i++) {
+                ctreBooleans.push((byte & (1 << i)) !== 0);
+              }
+            });
+
+            // Get currents
+            for (let i = 0; i < 16; i++) {
+              let readPosition = Math.floor(i / 6) * 64 + (i % 6) * 10;
+              let value = 0;
+              for (let i = 0; i < 8; i++) {
+                value += ctreBooleans[readPosition + i] ? Math.pow(2, i) : 0;
+              }
+              currents.push(value / 8);
             }
-          });
 
-          // Get currents
-          let currentPositions = [0, 10, 20, 30, 40, 50, 64, 74, 84, 94, 104, 114, 128, 138, 148, 158];
-          currentPositions.forEach((currentPosition) => {
-            let value = 0;
-            for (let i = 0; i < 8; i++) {
-              value += booleanData[currentPosition + i] ? Math.pow(2, i) : 0;
-            }
-            currents.push(value / 16);
-          });
+            // Skip extra metadata
+            position += 21 + 3;
+            break;
 
-          // Skip extra metadata
-          position += 21 + 3;
+          case PowerDistributionType.None:
+            break;
+        }
+        entry.powerDistributionCurrents = currents;
+
+        // Send entry
+        callback(entry);
+
+        // Adjust timestamp and check for end of log
+        timestamp += this.PERIOD_SECS;
+        if (position >= this.data.length) {
           break;
-
-        case PowerDistributionType.None:
-          position += 4;
-          break;
+        }
       }
-      entry.powerDistributionCurrents = currents;
-
-      // Send entry
-      callback(entry);
-
-      // Adjust timestamp and check for end of log
-      timestamp += this.PERIOD_SECS;
-      if (position >= this.data.length) {
-        break;
-      }
+    } catch (error) {
+      console.error("Problem reading DS log file, see below");
+      console.error(error);
     }
   }
 }
