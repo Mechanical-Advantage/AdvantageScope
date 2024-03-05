9e71486d69113fc4b2ff280b029ffda574b03981
m10653 <m10653@users.noreply.github.com>
2024-03-05 13:46:10 +0000

    Remove Unused print

--- src/shared/log/LogField.ts
+++ src/shared/log/LogField.ts
@@ -67,7 +67,6 @@ export default class LogField {
 
     var itr = this.data.lowerBound({ timestamp: start, values: 0 });
     while (itr.isAccessible()) {
-      console.log(itr.pointer);
       if (itr.pointer.timestamp > end) {
         break;
       }
