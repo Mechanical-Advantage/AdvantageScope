d538ec159b53976904eaf63624fec8fea9f675cc
m10653 <m10653@users.noreply.github.com>
2024-03-06 08:59:58 +0000

    Removed Dev build settings

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,7 +2,7 @@ import commonjs from "@rollup/plugin-commonjs";
 import json from "@rollup/plugin-json";
 import nodeResolve from "@rollup/plugin-node-resolve";
 import replace from "@rollup/plugin-replace";
-import typescript from "rollup-plugin-typescript2";
+import typescript from "@rollup/plugin-typescript";
 import fs from "fs";
 import cleanup from "rollup-plugin-cleanup";
 import replaceRegEx from "rollup-plugin-re";
@@ -16,10 +16,9 @@ function bundle(input, output, isMain, external = []) {
       format: isMain ? "cjs" : "es"
     },
     context: "this",
-    treeshake: true,
     external: external,
     plugins: [
-      typescript({ check: false, sourcemap: false }),
+      typescript(),
       nodeResolve(),
       commonjs(),
       cleanup(),
