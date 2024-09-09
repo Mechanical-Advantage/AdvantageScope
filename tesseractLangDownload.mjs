46d33618b7d3f1d7bc717427bad014b7a6310844
Jonah <47046556+jwbonner@users.noreply.github.com>
2024-09-09 17:41:49 -0400

    Failed attempt to fix tesseract

--- tesseractLangDownload.mjs
+++ tesseractLangDownload.mjs
@@ -1,6 +1,18 @@
 import download from "download";
+import fs from "fs";
+import gunzipFile from "gunzip-file";
 
-const FILENAME = "eng.traineddata.gz";
-const URL = "https://cdn.jsdelivr.net/npm/@tesseract.js-data/eng/4.0.0_best_int/" + FILENAME;
+const FILENAME_GZIP = "eng.traineddata.gz";
+const FILENAME = "eng.traineddata";
+const URL = "https://cdn.jsdelivr.net/npm/@tesseract.js-data/eng/4.0.0_best_int/" + FILENAME_GZIP;
 
-await download(URL, process.cwd(), { filename: FILENAME });
+// Download gzip
+await download(URL, process.cwd(), { filename: FILENAME_GZIP });
+
+// Unzip gzip
+await new Promise((resolve) => {
+  gunzipFile(FILENAME_GZIP, FILENAME, resolve);
+});
+
+// Delete gzip
+fs.unlinkSync(FILENAME_GZIP);
