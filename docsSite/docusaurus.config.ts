1e61713b45e72caaf7ea6a8531584586e36cbcf0
Jonah <47046556+jwbonner@users.noreply.github.com>
2024-09-15 19:08:58 -0400

    Update docs URL

--- docsSite/docusaurus.config.ts
+++ docsSite/docusaurus.config.ts
@@ -7,10 +7,10 @@ const config: Config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://ascope.littletonrobotics.org",
+  url: "https://mechanical-advantage.github.io/AdvantageScope",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/AdvantageScope/",
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
