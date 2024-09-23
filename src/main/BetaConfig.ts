13a1a95e2e7d4d992078b91fd69ee151486151b2
Jonah <47046556+jwbonner@users.noreply.github.com>
2024-09-23 16:29:00 -0400

    Fix beta expiration date

--- src/main/BetaConfig.ts
+++ src/main/BetaConfig.ts
@@ -8,7 +8,7 @@ import { APP_VERSION } from "./Constants";
 // Constants
 const BETA_CONFIG: BetaConfig | null = {
   year: "2025",
-  expiration: new Date(2024, 0, 4),
+  expiration: new Date(2025, 0, 4),
   surveyUrl:
     "https://docs.google.com/forms/d/e/1FAIpQLSe7xxOln2NUO6pNrGQAPAJ5W7HH2dcIfcCYYi0etTQQE5ORwg/viewform?usp=pp_url&entry.1466744914=__version__"
 };
