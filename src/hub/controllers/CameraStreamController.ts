067293fe331a7d65b089027cad32799985ad9d18
aaronleetw <58136818+aaronleetw@users.noreply.github.com>
2025-01-31 01:23:58 +0800

    added CameraStream tab

---
+++ src/hub/controllers/CameraStreamController.ts
@@ -0,0 +1,124 @@
+import { SourceListState } from "../../shared/SourceListConfig";
+import { getOrDefault } from "../../shared/log/LogUtil";
+import LoggableType from "../../shared/log/LoggableType";
+import { CameraStream, CameraStreamRendererCommand } from "../../shared/renderers/CameraStreamRenderer";
+import { createUUID } from "../../shared/util";
+import SourceList from "../SourceList";
+import CameraStreamController_Config from "./CameraStreamController_Config";
+import TabController from "./TabController";
+
+export default class CameraStreamController implements TabController {
+  UUID = createUUID();
+
+  private WIDTH: HTMLInputElement;
+  private HEIGHT: HTMLInputElement;
+  private FPS: HTMLInputElement;
+  private COMPRESSION: HTMLInputElement;
+  private COLUMNS: HTMLInputElement;
+
+  private sourceList: SourceList;
+  
+  public static constructUrl(baseUrl: string, resolution: { width: number, height: number }, fps: number, compression: number): string {
+    if (fps !== -1) baseUrl += `&fps=${fps}`;
+    if (compression !== -1) baseUrl += `&compression=${compression}`;
+    if (resolution.width !== -1 && resolution.height !== -1) baseUrl += `&resolution=${resolution.width}x${resolution.height}`;
+    return baseUrl;
+  }
+
+  constructor(root: HTMLElement) {
+    this.sourceList = new SourceList(root.firstElementChild as HTMLElement, CameraStreamController_Config, []);
+    let settings = root.getElementsByClassName("camerastream-settings")[0] as HTMLElement;
+    this.WIDTH = settings.getElementsByClassName("resolution-width")[0] as HTMLInputElement;
+    this.HEIGHT = settings.getElementsByClassName("resolution-height")[0] as HTMLInputElement;
+    this.FPS = settings.getElementsByClassName("fps")[0] as HTMLInputElement;
+    this.COMPRESSION = settings.getElementsByClassName("compression")[0] as HTMLInputElement;
+    this.COLUMNS = settings.getElementsByClassName("columns")[0] as HTMLInputElement;
+
+    // Enforce number ranges (> 0 integers)
+    [this.WIDTH, this.HEIGHT, this.COLUMNS].forEach((input) => {
+      input.addEventListener("change", () => {
+        if (Number(input.value) % 1 !== 0) input.value = Math.round(Number(input.value)).toString();
+        if (Number(input.value) <= 0) input.value = "1";
+      });
+    });
+    // Enforce number ranges (100 > x > -1 integers)
+    [this.FPS, this.COMPRESSION].forEach((input) => {
+      input.addEventListener("change", () => {
+        if (Number(input.value) % 1 !== 0) input.value = Math.round(Number(input.value)).toString();
+        if (Number(input.value) < -1) input.value = "-1";
+        if (Number(input.value) > 100) input.value = "100";
+      });
+    });
+  }
+
+  getCommand(): CameraStreamRendererCommand {
+    let time = window.selection.getRenderTime();
+    if (time === null) time = window.log.getTimestampRange()[1];
+
+    let sources = this.sourceList.getState(true);
+    return {
+      columns: Number(this.COLUMNS.value),
+      streams: sources.map((source) => {
+        if (source.type === "stream") {
+          let baseUrl = getOrDefault(window.log, source.logKey, LoggableType.String, time, "", this.UUID);
+          baseUrl = baseUrl.replace("mjpg:", "");
+          let resolution = { width: Number(this.WIDTH.value), height: Number(this.HEIGHT.value) };
+          let fps = Number(this.FPS.value);
+          let compression = Number(this.COMPRESSION.value);
+          return {
+            source: baseUrl,
+            url: CameraStreamController.constructUrl(baseUrl, resolution, fps, compression),
+          }
+        }
+      }).filter((url): url is CameraStream => url !== undefined)
+    }
+  }
+
+  saveState(): unknown {
+    return {
+      sources: this.sourceList.getState(),
+      width: Number(this.WIDTH.value),
+      height: Number(this.HEIGHT.value),
+      fps: Number(this.FPS.value),
+      compression: Number(this.COMPRESSION.value),
+      columns: Number(this.COLUMNS.value),
+    };
+  }
+
+  restoreState(state: unknown): void {
+    if (typeof state !== "object" || state === null) return;
+
+    if ("sources" in state) {
+      this.sourceList.setState(state.sources as SourceListState);
+    }
+    if ("width" in state && typeof state.width === "number") {
+      this.WIDTH.value = state.width.toString();
+    }
+    if ("height" in state && typeof state.height === "number") {
+      this.HEIGHT.value = state.height.toString();
+    }
+    if ("fps" in state && typeof state.fps === "number") {
+      this.FPS.value = state.fps.toString();
+    }
+    if ("compression" in state && typeof state.compression === "number") {
+      this.COMPRESSION.value = state.compression.toString();
+    }
+    if ("columns" in state && typeof state.columns === "number") {
+      this.COLUMNS.value = state.columns.toString();
+    }
+  }
+
+  refresh(): void {
+    this.sourceList.refresh();
+  }
+
+  newAssets(): void {}
+
+  getActiveFields(): string[] {
+    return this.sourceList.getActiveFields();
+  }
+
+  showTimeline(): boolean {
+    return false;
+  }
+}
---
+++ src/shared/renderers/CameraStreamRenderer.ts
@@ -0,0 +1,87 @@
+import TabRenderer from "./TabRenderer";
+
+export default class CameraStreamRenderer implements TabRenderer {
+  private VIEWER_CONTAINER: HTMLDivElement;
+  private VIEWERS: HTMLDivElement[] = [];
+
+  constructor(root: HTMLElement) {
+    this.VIEWER_CONTAINER = root.getElementsByClassName("camerastream-container")[0] as HTMLDivElement;
+    this.VIEWERS = [];
+  }
+
+  getAspectRatio(): number | null {
+    return null;
+  }
+
+  private clearStreams(): void {
+    this.VIEWERS.forEach((stream) => {
+      let img = stream.getElementsByTagName("img")[0]; // Stop loading the image
+      img.src = "";
+      stream.remove();
+    });
+    this.VIEWERS = [];
+  }
+
+  render(command: CameraStreamRendererCommand): void {
+    if (this.VIEWER_CONTAINER.style.gridTemplateColumns !== `repeat(${command.columns}, 1fr)`) {
+      this.VIEWER_CONTAINER.style.gridTemplateColumns = `repeat(${command.columns}, 1fr)`;
+    }
+    if (this.VIEWER_CONTAINER.style.gridTemplateRows !== `repeat(${Math.ceil(command.streams.length / command.columns)}, minmax(0, 1fr))`) {
+      this.VIEWER_CONTAINER.style.gridTemplateRows = `repeat(${Math.ceil(command.streams.length / command.columns)}, minmax(0, 1fr))`;
+    }
+
+    var streamsChanged = false;
+
+    if (this.VIEWERS.length !== command.streams.length) {
+      // Index mismatch
+      streamsChanged = true;
+    } else {
+      // We check one by one if the streams have changed
+      this.VIEWERS.forEach((stream, index) => {
+        let span = stream.getElementsByTagName("span")[0];
+        if (span.textContent !== command.streams[index].url) {
+          streamsChanged = true;
+        }
+      });
+    }
+
+    if (streamsChanged) {
+      this.clearStreams();
+
+      // Add new streams
+      command.streams.forEach((stream) => {
+        let viewer = this.VIEWER_CONTAINER.appendChild(document.createElement("div"));
+        viewer.className = "camerastream-viewer";
+        let sourceText = viewer.appendChild(document.createElement("p"));
+        sourceText.textContent = stream.source;
+        let img = viewer.appendChild(document.createElement("img"));
+        img.src = stream.url;
+        img.onerror = () => {
+          img.src = "symbols/exclamationmark.triangle.fill.svg";
+        }
+        // add a hidden span to store the full url
+        let urlSpan = viewer.appendChild(document.createElement("span"));
+        urlSpan.textContent = stream.url;
+        urlSpan.style.display = "none";
+
+        this.VIEWERS.push(viewer);
+      });
+    }
+  }
+
+  saveState(): unknown {
+    return null;
+  }
+
+  restoreState(state: unknown): void {}
+}
+
+export type CameraStream = {
+  url: string,
+  source: string
+}
+
+export type CameraStreamRendererCommand = {
+  streams: CameraStream[],
+  columns: number
+};
