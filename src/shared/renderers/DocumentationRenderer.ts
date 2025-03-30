771bd0559be48b1818b83830b88fbc34519c1a3c
Jonah <47046556+jwbonner@users.noreply.github.com>
2025-03-30 14:29:13 -0400

    Maintain documentation scroll position when switching tabs (closes #357)

--- src/shared/renderers/DocumentationRenderer.ts
+++ src/shared/renderers/DocumentationRenderer.ts
@@ -6,6 +6,8 @@ export default class DocumentationRenderer implements TabRenderer {
 
   private stateQueue: string | null = null;
   private loaded = false;
+  private scrollPosition = 0;
+  private shouldResetScroll = false;
 
   constructor(content: HTMLElement) {
     this.CONTAINER = content.getElementsByClassName("documentation-container")[0] as HTMLElement;
@@ -13,6 +15,15 @@ export default class DocumentationRenderer implements TabRenderer {
     this.IFRAME.addEventListener("load", () => {
       this.loaded = true;
     });
+
+    // Periodic function to record when tab is hidden
+    let periodic = () => {
+      if (this.CONTAINER.getBoundingClientRect().height === 0) {
+        this.shouldResetScroll = true;
+      }
+      window.requestAnimationFrame(periodic);
+    };
+    window.requestAnimationFrame(periodic);
   }
 
   saveState(): unknown {
@@ -31,9 +42,20 @@ export default class DocumentationRenderer implements TabRenderer {
   }
 
   render(_: unknown): void {
+    // Navigate to initial page when loaded
     if (this.stateQueue !== null && this.loaded) {
       this.IFRAME.contentWindow!.location.hash = this.stateQueue;
       this.stateQueue = null;
+    }
+
+    // Update scroll location
+    if (this.loaded && this.IFRAME.contentWindow !== null) {
+      if (this.shouldResetScroll) {
+        this.IFRAME.contentWindow.scrollTo(0, this.scrollPosition);
+        this.shouldResetScroll = false;
+      } else {
+        this.scrollPosition = this.IFRAME.contentWindow.scrollY;
+      }
     }
   }
 }
