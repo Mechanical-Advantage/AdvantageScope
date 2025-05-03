863c0acb4ca1360413ec667e8c6f431726211df0
j5155 <54331556+j5155@users.noreply.github.com>
2025-05-03 15:40:58 -0800

    autoformat

--- src/hub/dataSources/ftcdashboard/configReducer.ts
+++ src/hub/dataSources/ftcdashboard/configReducer.ts
@@ -1,92 +1,80 @@
-import {
-  ConfigAction,
-  ConfigState,
-  ConfigVar,
-  ConfigVarState,
-  ReceiveConfigAction,
-  RefreshConfigAction,
-  SaveConfigAction,
-  UpdateConfigAction
-} from "./configTypes";
+import { ConfigAction, ConfigState, ConfigVar, ConfigVarState } from "./configTypes";
 
 function inflate(v: ConfigVar): ConfigVarState {
-  if (v.__type === 'custom') {
+  if (v.__type === "custom") {
     const value = v.__value;
     if (value === null) {
       return {
-        __type: 'custom',
-        __value: null,
+        __type: "custom",
+        __value: null
       };
     } else {
       return {
-        __type: 'custom',
+        __type: "custom",
         __value: Object.keys(value).reduce(
           (acc, key) => ({
             ...acc,
-            [key]: inflate(value[key]),
+            [key]: inflate(value[key])
           }),
-          {},
-        ),
+          {}
+        )
       };
     }
   } else {
     return {
       ...v,
       __newValue: v.__value,
-      __valid: true,
+      __valid: true
     };
   }
 }
 
 // merge modified, matching members of base into latest
-function mergeModified(
-  base: ConfigVarState,
-  latest: ConfigVar,
-): ConfigVarState {
-  if (base.__type === 'custom' && latest.__type === 'custom') {
+function mergeModified(base: ConfigVarState, latest: ConfigVar): ConfigVarState {
+  if (base.__type === "custom" && latest.__type === "custom") {
     const latestValue = latest.__value;
     if (latestValue === null) {
       return {
-        __type: 'custom',
-        __value: null,
+        __type: "custom",
+        __value: null
       };
     } else {
       return {
-        __type: 'custom',
+        __type: "custom",
         __value: Object.keys(latestValue).reduce(
           (acc, key) =>
             base.__value !== null && key in base.__value
               ? {
                   ...acc,
-                  [key]: mergeModified(base.__value[key], latestValue[key]),
+                  [key]: mergeModified(base.__value[key], latestValue[key])
                 }
               : {
                   ...acc,
-                  [key]: inflate(latestValue[key]),
+                  [key]: inflate(latestValue[key])
                 },
-          {},
-        ),
+          {}
+        )
       };
     }
   } else if (
-    base.__type === 'enum' &&
-    latest.__type === 'enum' &&
+    base.__type === "enum" &&
+    latest.__type === "enum" &&
     base.__enumClass === latest.__enumClass &&
     base.__value !== base.__newValue
   ) {
     return {
       ...base,
-      __value: latest.__value,
+      __value: latest.__value
     };
   } else if (
     base.__type === latest.__type &&
-    /* type checker reminder */ base.__type !== 'custom' &&
-    latest.__type !== 'custom' &&
+    /* type checker reminder */ base.__type !== "custom" &&
+    latest.__type !== "custom" &&
     base.__value !== base.__newValue
   ) {
     return {
       ...base,
-      __value: latest.__value,
+      __value: latest.__value
     };
   } else {
     return inflate(latest);
@@ -94,59 +82,56 @@ function mergeModified(
 }
 
 function revertModified(state: ConfigVarState): ConfigVarState {
-  if (state.__type === 'custom') {
+  if (state.__type === "custom") {
     const value = state.__value;
     if (value === null) {
       return {
-        __type: 'custom',
-        __value: null,
+        __type: "custom",
+        __value: null
       };
     } else {
       return {
-        __type: 'custom',
+        __type: "custom",
         __value: Object.keys(value).reduce(
           (acc, key) => ({
             ...acc,
-            [key]: inflate(value[key]),
+            [key]: inflate(value[key])
           }),
-          {},
-        ),
+          {}
+        )
       };
     }
   } else {
     return {
       ...state,
-      __newValue: state.__value,
+      __newValue: state.__value
     };
   }
 }
 
 export const initialState: ConfigState = {
   configRoot: {
-    __type: 'custom',
-    __value: {},
-  },
+    __type: "custom",
+    __value: {}
+  }
 };
 
-const configReducer = (
-  state: ConfigState = initialState,
-  action: ConfigAction,
-): ConfigState => {
+const configReducer = (state: ConfigState = initialState, action: ConfigAction): ConfigState => {
   switch (action.type) {
-    case 'RECEIVE_CONFIG':
+    case "RECEIVE_CONFIG":
       return {
         ...state,
-        configRoot: mergeModified(state.configRoot, action.configRoot),
+        configRoot: mergeModified(state.configRoot, action.configRoot)
       };
-    case 'UPDATE_CONFIG':
+    case "UPDATE_CONFIG":
       return {
         ...state,
-        configRoot: action.configRoot,
+        configRoot: action.configRoot
       };
-    case 'REFRESH_CONFIG':
+    case "REFRESH_CONFIG":
       return {
         ...state,
-        configRoot: revertModified(state.configRoot),
+        configRoot: revertModified(state.configRoot)
       };
     default:
       return state;
--- src/hub/dataSources/ftcdashboard/configTypes.ts
+++ src/hub/dataSources/ftcdashboard/configTypes.ts
@@ -2,38 +2,38 @@ export type ConfigVar = CustomVar | BasicVar;
 export type ConfigVarState = CustomVarState | BasicVarState;
 
 export type CustomVar = {
-  __type: 'custom';
+  __type: "custom";
   __value: Record<string, ConfigVar> | null;
 };
 
 export type CustomVarState = {
-  __type: 'custom';
+  __type: "custom";
   __value: Record<string, ConfigVarState> | null;
 };
 
 export type BasicVar =
   | {
-      __type: 'enum';
+      __type: "enum";
       // only string is actualy present, but this helps treat vars uniformly
       __value: boolean | number | string | null;
       __enumClass: string;
       __enumValues: string[];
     }
   | {
-      __type: 'boolean' | 'int' | 'long' | 'float' | 'double' | 'string';
+      __type: "boolean" | "int" | "long" | "float" | "double" | "string";
       __value: boolean | number | string | null;
     };
 
 export type BasicVarState = (
   | {
-      __type: 'enum';
+      __type: "enum";
       __value: boolean | number | string | null;
       __newValue: boolean | number | string | null;
       __enumClass: string;
       __enumValues: string[];
     }
   | {
-      __type: 'boolean' | 'int' | 'long' | 'float' | 'double' | 'string';
+      __type: "boolean" | "int" | "long" | "float" | "double" | "string";
       __value: boolean | number | string | null;
       __newValue: boolean | number | string | null;
     }
@@ -46,21 +46,21 @@ export type ConfigState = {
 };
 
 export type ReceiveConfigAction = {
-  type: 'RECEIVE_CONFIG';
+  type: "RECEIVE_CONFIG";
   configRoot: ConfigVar;
 };
 
 export type UpdateConfigAction = {
-  type: 'UPDATE_CONFIG';
+  type: "UPDATE_CONFIG";
   configRoot: ConfigVarState;
 };
 
 export type SaveConfigAction = {
-  type: 'SAVE_CONFIG';
+  type: "SAVE_CONFIG";
   configDiff: ConfigVar;
 };
 export type RefreshConfigAction = {
-  type: 'REFRESH_CONFIG';
+  type: "REFRESH_CONFIG";
 };
 
 export type ConfigAction = ReceiveConfigAction | UpdateConfigAction | SaveConfigAction | RefreshConfigAction;
