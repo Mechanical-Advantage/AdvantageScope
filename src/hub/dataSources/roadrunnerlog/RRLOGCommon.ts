37b625152cbf3b734a1ffe86491ceffeaca28441
j5155 <54331556+j5155@users.noreply.github.com>
2025-05-24 16:55:35 -0800

    Autoformat

--- src/hub/dataSources/roadrunnerlog/RRLOGCommon.ts
+++ src/hub/dataSources/roadrunnerlog/RRLOGCommon.ts
@@ -1,3 +1,10 @@
+// Copyright (c) 2021-2025 Littleton Robotics
+// http://github.com/Mechanical-Advantage
+//
+// Use of this source code is governed by a BSD
+// license that can be found in the LICENSE file
+// at the root directory of this project.
+
 export type MessageSchema = StructSchema | PrimitiveSchema | EnumSchema;
 export class StructSchema {
   fields = new Map<string, MessageSchema>();
