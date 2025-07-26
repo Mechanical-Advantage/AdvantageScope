772132cfe0aa38979c2a2040c06832fdd9ca4a6a
me-it-is <188520719+me-it-is@users.noreply.github.com>
2025-07-26 13:41:47 -0700

    fix emscripten

--- wasmCompile.mjs
+++ wasmCompile.mjs
@@ -51,7 +51,7 @@ try {
       outPath = "'bundles/hub$wpilogIndexer.js' ";
     }
     exec(
-      `emcc ${inPath} -o ${outPath} -sEXPORTED_FUNCTIONS=_run,_malloc -sALLOW_MEMORY_GROWTH -sMAXIMUM_MEMORY=4294967296 -O3`,
+      `emcc ${inPath} -o ${outPath} -sEXPORTED_FUNCTIONS=_run,_malloc -sALLOW_MEMORY_GROWTH -sMAXIMUM_MEMORY=4294967296 -O3 -sEXPORTED_RUNTIME_METHODS=HEAPU8,HEAPF64,HEAPU32`,
       (error, stdout, stderr) => {
         console.log(stdout);
         console.error(stderr);
