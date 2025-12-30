f65528ae14cb4f1a7972d8b8a0073e694c76350c
Jonah <47046556+jwbonner@users.noreply.github.com>
2025-12-30 11:36:43 -0500

    Disable beta configuration

--- src/main/betaConfig.ts
+++ src/main/betaConfig.ts
@@ -5,13 +5,7 @@
 // license that can be found in the LICENSE file
 // at the root directory of this project.
 
-export const BETA_CONFIG: BetaConfig | null = {
-  year: "2026",
-  isAlpha: false,
-  expiration: new Date(2026, 0, 10),
-  surveyUrl:
-    "https://docs.google.com/forms/d/e/1FAIpQLSeiBaYQ0CME-Fjt2G3d4uJ1cKOQfj8NPTPh7mNvA_ZQFQwGHw/viewform?usp=header"
-};
+export const BETA_CONFIG: BetaConfig | null = null;
 
 export type BetaConfig = {
   year: string;
