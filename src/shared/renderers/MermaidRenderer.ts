6919c33cc7b2f018faa555d22904815c8cbda74d
Daniel1464 <Daniel_Chen@caryacademy.org>
2026-05-28 12:17:00 -0400

    Let's go it actually works!!!!!

--- src/shared/renderers/MermaidRenderer.ts
+++ src/shared/renderers/MermaidRenderer.ts
@@ -5,20 +5,28 @@
 // license that can be found in the LICENSE file
 // at the root directory of this project.
 
-import mermaid from "mermaid";
+import { Graphviz } from "@hpcc-js/wasm-graphviz";
 import TabRenderer from "./TabRenderer";
+import { mermaidToDotGraph } from "./mermaid/mermaidToDotGraph";
+
+let graphVizImpl: Graphviz | null = null;
+async function loadGraphViz(): Promise<Graphviz> {
+  if (graphVizImpl === null) {
+    graphVizImpl = await Graphviz.load();
+  }
+  return graphVizImpl;
+}
 
 export default class MermaidRenderer implements TabRenderer {
   private CONTAINER: HTMLElement;
   private lastDiagram: string | null = null;
-  private lastRenderedDiagram: string | null = null;
+  private lastWidth: number | null = null;
+  private lastHeight: number | null = null;
+  private lastDarkMode: boolean | null = null;
 
   constructor(root: HTMLElement) {
     this.CONTAINER = root.getElementsByClassName("mermaid-container")[0] as HTMLElement;
-    mermaid.initialize({
-      startOnLoad: false,
-      theme: window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "default"
-    });
+    loadGraphViz()
   }
 
   saveState(): unknown {
@@ -32,18 +40,29 @@ export default class MermaidRenderer implements TabRenderer {
   }
 
   async render(command: MermaidRendererCommand) {
-    if (command.diagram === this.lastDiagram) return;
+    const darkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
+    if (
+      command.diagram === this.lastDiagram &&
+      this.lastWidth === this.CONTAINER.clientWidth &&
+      this.lastHeight === this.CONTAINER.clientHeight &&
+      this.lastDarkMode === darkMode
+    ) return;
     this.lastDiagram = command.diagram;
 
     if (command.diagram === null || command.diagram.trim() === "") {
       this.CONTAINER.innerHTML = "";
-      this.lastRenderedDiagram = null;
     } else {
-      if (command.diagram === this.lastRenderedDiagram) return;
+      console.log("WIDTH: " + this.CONTAINER.clientWidth + " HEIGHT: " + this.CONTAINER.clientHeight);
       try {
-        const { svg } = await mermaid.render("mermaid-" + Math.random().toString(36).substring(7), command.diagram);
+        const width = this.CONTAINER.clientWidth - 5;
+        const height = this.CONTAINER.clientHeight - 5;
+        const darkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
+        const dotGraph = mermaidToDotGraph(width, height, command.diagram, darkMode);
+        const svg = (await loadGraphViz()).dot(dotGraph, "svg");
         this.CONTAINER.innerHTML = svg;
-        this.lastRenderedDiagram = command.diagram;
+        this.lastWidth = this.CONTAINER.clientWidth;
+        this.lastHeight = this.CONTAINER.clientHeight;
+        this.lastDarkMode = darkMode;
       } catch (e) {
         this.CONTAINER.innerText = "Error rendering Mermaid diagram: " + (e as Error).message;
       }
