31657d4042f19d4bc8fc80e07e1d444f355bee05
Jonah <47046556+jwbonner@users.noreply.github.com>
2026-07-02 17:59:19 -0400

    Disable beta expiration logic in Lite DS

--- src/main/lite/main.ts
+++ src/main/lite/main.ts
@@ -1349,6 +1349,7 @@ window.addEventListener("load", () => {
   if (isBeta()) {
     if (isBetaExpired()) {
       if (
+        DISTRIBUTION != Distribution.LiteDS &&
         confirm(
           `The AdvantageScope Lite ${
             isAlpha() ? "alpha" : "beta"
